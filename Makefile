71251a2601fb2895f9ad7967d6ca28697319659c
Bill Cox <waywardgeek@google.com>
2016-01-11 16:29:03 -0800

    Added predict16

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: predict predict8
+all: predict predict8 predict16
 
 predict: predict.c
 	gcc -Wall -O3 predict.c -o predict -lm
 
 predict8: predict8.c
 	gcc -Wall -g predict8.c -o predict8 -lm
+
+predict16: predict16.c
+	gcc -Wall -g predict16.c -o predict16 -lm
--- predict16.c
+++ predict16.c
@@ -14,26 +14,42 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Unable to open file %s for reading\n", argv[2]);
         return 1;
     }
-    uint32_t next[256][256] = {{0,},};
-    uint32_t total[256] = {0,};
+    static uint32_t next[256][256][256] = {{{0,},},};
+    static uint32_t total[256][256] = {{0,},};
     uint8_t prevByte = 0;
+    uint8_t prevPrevByte = 0;
     int byte;
     float surprise = 1.0;
     uint64_t totalBytes = 0;
     uint64_t bitsOfEntropy = 0;
     while((byte = getc(datafile)) != EOF) {
-        next[prevByte][byte]++;
-        total[prevByte]++;
-        surprise *= next[prevByte][byte]/(float)total[prevByte];
+        next[prevPrevByte][prevByte][byte]++;
+        total[prevPrevByte][prevByte]++;
+        surprise *= next[prevPrevByte][prevByte][byte]/(float)total[prevPrevByte][prevByte];
         while(surprise < 0.5f) {
             bitsOfEntropy++;
             surprise *= 2.0f;
         }
         totalBytes++;
+        prevPrevByte = prevByte;
         prevByte = byte;
     }
     fclose(datafile);
     printf("There seems to be at most %f bits per byte in this sample\n", (float)bitsOfEntropy/totalBytes);
     printf("Based on %lu total samples\n", totalBytes);
+    uint32_t i, j, k;
+    uint32_t numBins = 0;
+    uint64_t totalHits = 0;
+    for(i = 0; i < 256; i++) {
+        for(j = 0; j < 256; j++) {
+            for(k = 0; k < 256; k++) {
+                if(next[i][j][k] > 0) {
+                    numBins++;
+                    totalHits += next[i][j][k];
+                }
+            }
+        }
+    }
+    printf("num bins: %u, average hits per bin: %.2f\n", numBins, (float)totalHits/numBins);
     return 0;
 }
